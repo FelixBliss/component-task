c47aac070303f8ec25db31ad7d4bd4be4c944878
FelixBliss <43016973+FelixBliss@users.noreply.github.com>
2026-08-30 16:20:12 +0000

    Update rgn.ts

--- web/src/data/procedureData/rgn.ts
+++ web/src/data/procedureData/rgn.ts
@@ -1,6 +1,7 @@
-import type { Procedure } from "./procedureTypes";
 import { rgnBatch01 } from "./rgnBatches/rgnBatch01";
+import { rgnBatch02 } from "./rgnBatches/rgnBatch02";
 
 export const rgnProcedures: Procedure[] = [
   ...rgnBatch01,
+  ...rgnBatch02,
 ];
