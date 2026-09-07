b2081429b01270deb55eeab102768db041062f75
FelixBliss <43016973+FelixBliss@users.noreply.github.com>
2026-09-07 21:54:46 +0000

    RM and RGN PROCEDURE

--- web/src/data/procedures.ts
+++ web/src/data/procedures.ts
@@ -1,8 +1,10 @@
 import type { Procedure } from "./procedureTypes";
 import { rgnProcedures } from "./procedureData/rgn";
+import { rmProcedures } from "./procedureData/rm";
 
 export type { Procedure } from "./procedureTypes";
 
 export const procedures: Procedure[] = [
   ...rgnProcedures,
+  ...rmProcedures,
 ];
