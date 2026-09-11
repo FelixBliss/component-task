4844be59634e3d505c8f81d2f5bd315102cbbbcf
FelixBliss <43016973+FelixBliss@users.noreply.github.com>
2026-09-11 17:49:08 +0000

    Rename 'Registered Mental Health Nursing' to 'Mental Health Nursing'

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -45,7 +45,7 @@ const procedureCategories: Category[] = [
   },
   {
     icon: "🧠",
-    name: "Registered Mental Health Nursing (RMN)",
+    name: "Mental Health Nursing (RMN)",
     description: "Mental health nursing procedures",
   },
   {
