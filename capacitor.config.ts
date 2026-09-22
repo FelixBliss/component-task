be171b82239e9d5505af0b26cb430da2a45e2732
FelixBliss <43016973+FelixBliss@users.noreply.github.com>
2026-09-22 02:53:36 +0000

    Configure root Capacitor project for web assets

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.my.componenttask',
   appName: 'Component Task',
-  webDir: 'dist'
+  webDir: 'web/dist'
 };
 
 export default config;
